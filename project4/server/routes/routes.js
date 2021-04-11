017c6d01e48e29a6bcbcdeb6b2361f919a2d070b
mark <yw228@byu.edu>
2021-04-11 14:51:23 -0600

    Update routes.js

--- project4/server/routes/routes.js
+++ project4/server/routes/routes.js
@@ -8,7 +8,7 @@ const multer = require('multer');
 
 let storage= multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, "./uploads");
+        cb(null, "/var/www/c4.rubyshark.com/uploads");
     },
     filename: function(req, file ,cb) {
         cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname);
